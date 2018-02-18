f8d624d320af8d42b7889ccf625722dfab1b4449
ravbetsky <ravbetsky@gmail.com>
2018-02-18 18:01:10 +0400

    add progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,7 +1,21 @@
 import { cons } from 'hexlet-pairs';
+import { getRandomInt } from '../utils';
 import gameConstructor from '../index';
 
 export default () => {
   const rules = 'What number is missing in this progression?';
-  gameConstructor(rules);
+  const makeQuestion = () => {
+    const length = 10;
+    const first = getRandomInt(0, 10);
+    const delta = getRandomInt(1, 10);
+    const missingIndex = getRandomInt(0, length - 1);
+    const correct = first + (missingIndex * delta);
+    let question = '';
+    for (let i = 0; i < length; i += 1) {
+      const next = i === missingIndex ? '..' : first + (i * delta);
+      question += `${next} `;
+    }
+    return cons(question, correct);
+  };
+  gameConstructor(rules, makeQuestion);
 };
