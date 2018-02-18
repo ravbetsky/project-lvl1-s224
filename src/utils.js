a4a770881051dc69fcacbd1abd2218ee85eef616
ravbetsky <ravbetsky@gmail.com>
2018-02-18 16:07:17 +0400

    fix sort func utils

--- src/utils.js
+++ src/utils.js
@@ -23,7 +23,7 @@ export const getExMax = extrema => cdr(extrema);
 
 export const isSortedNum = (n) => {
   const str = String(n);
-  for (let i = 1; i < str.length - 2; i += 1) {
+  for (let i = 1; i < str.length - 1; i += 1) {
     if (Number(str[i - 1]) > Number(str[i]) || Number(str[i]) > Number(str[i + 1])) {
       return false;
     }
