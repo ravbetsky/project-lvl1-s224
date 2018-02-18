ffb5e320b4f19f0f49408752eb9173a1f9f33504
ravbetsky <ravbetsky@gmail.com>
2018-02-18 17:47:52 +0400

    add progression game files

---
+++ src/games/progression.js
@@ -0,0 +1,7 @@
+import { cons } from 'hexlet-pairs';
+import gameConstructor from '../index';
+
+export default () => {
+  const rules = 'What number is missing in this progression?';
+  gameConstructor(rules);
+};
